496d035d5c824be3d5481e20585058a5745874cf
AsifSevatar <aassifabbasov@gmail.com>
2023-02-21 15:52:13 +0400

    add script js

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,21 @@
-'use strict'
+// 'use strict'
 
-let name = prompt("What's you name?", "");
-alert(name);
+const numberofFilms = +prompt('Сколько фильмов вы уже прсомотрели?', '');
+
+const personalMovieDB = {
+    count: numberofFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', ''); 
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
